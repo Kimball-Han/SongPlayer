7b02fad1232702cff5e03ca3bb27eea07231b482
qianfeng01 <qianfeng01@Kimball.local>
2015-04-18 09:55:54 +0800

    Initial Commit

---
+++ SongPlayer/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SongPlayer
+//
+//  Created by qianfeng01 on 15-4-18.
+//  Copyright (c) 2015年 qianfeng01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SongPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SongPlayer
+//
+//  Created by qianfeng01 on 15-4-18.
+//  Copyright (c) 2015年 qianfeng01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
